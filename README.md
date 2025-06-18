7bf6a59c3d38fee46bfc641c05588113c496f905
Travis Caruth <tcaruth@redstonecontentsolutions.com>
2025-06-18 17:09:14 -0500

    create readme. first version release

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # apex-documentation-userscript
-This userscript injects documentation into the built in spotlight search in APEX 24.2
+This userscript injects documentation into the built in spotlight search in APEX 21.1+. It has only been tested in 24.2.5.
+
+Please note due to the many different hosting options for APEX, the script requests the ability to run on any page with /ords/r/. This may include non-builder pages.
+
+## DISCLAIMER:
+This code is unofficial and is not supported by Oracle, the APEX team, or myself. It is provided "as is" without warranty of any kind, either express or implied. Use of this code is at your own risk. The authors and distributors accept no responsibility for any consequences arising from its use.
+
+## Installation:
+
+1. Install Violetmonkey, Tampermonkey, or similar userscript manager
+2. Install the userscript via URL: `https://raw.githubusercontent.com/tcaruth/apex-documentation-userscript/refs/heads/main/script.js`
+3. Optionally, enable auto updates. The script will update automatically when a new version is released.
+
+## Usage:
+
+1. Open any APEX builder page
+2. Click the search bar to open the spotlight search
+3. Type your search term, including any PL/SQL package procedure or function
+4. Navigate with arrow keys or click on any result to open the documentation in a new tab
+
+## To Do:
+
+- Add support for the javascript apis. Will require different processing to obtain and parse links.
+- Find a way to directly pull the PL/SQL TOC from the documentation
--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 // ==UserScript==
-// @name         Intercept APEX AJAX
-// @namespace    http://tampermonkey.net/
+// @name         APEX Documentation Spotlight Search
+// @namespace    https://github.com/tcaruth
 // @version      1.0
-// @description  Intercept and modify AJAX responses for a specific APEX endpoint
+// @description  Intercept and modify spotlight search results in APEX to include PL/SQL documentation
 // @downloadURL  https://raw.githubusercontent.com/tcaruth/apex-documentation-userscript/refs/heads/main/script.js
-// @match        *://*/ords*
+// @match        *://*/ords/r/*
+// @run-at       document-end
 // @author       Travis Caruth
 // @grant        none
+// @require      https://raw.githubusercontent.com/uzairfarooq/arrive/refs/heads/master/minified/arrive.min.js
 // ==/UserScript==
 
-(async function() {
+(async function () {
 	'use strict';
 
 	const tocUrl = `https://raw.githubusercontent.com/tcaruth/apex-documentation-userscript/refs/heads/main/toc/${apex.env.APEX_BASE_VERSION}.js`
@@ -19,7 +21,7 @@
 	const tocJson = JSON.parse(tocText.slice(7, -2));
 
 	console.log(tocJson);
-	
+
 	/**
 	 * Process the TOC and build an array of documentation objects
 	 * @param {Array} topics - Array of topic objects from the TOC
@@ -41,66 +43,40 @@
 
 			// Clean the title by removing HTML tags
 			const cleanTitle = topic.title.replace(/<[^>]*>/g, '');
-			
+
 			// Build the path
 			const currentPath = parentPath ? `${parentPath} / ${cleanTitle}` : cleanTitle;
-			
+
 			// Build the URL - make it absolute
 			const baseUrl = 'https://docs.oracle.com/en/database/oracle/apex/' + apex.env.APEX_BASE_VERSION + '/aeapi/';
 			const url = topic.href ? baseUrl + topic.href : '';
-			
+
 			// Create the doc object
 			const docObject = {
 				name: cleanTitle,
-				initials: getInitials(cleanTitle),
-				icon: '<span class="a-Spotlight-icon" aria-hidden="true"><span class="fa fa-question-circle-o" aria-hidden="true"></span></span>',
+				icon: '<span class="a-Spotlight-icon" aria-hidden="true"><span class="a-Icon icon-help" aria-hidden="true"></span></span>',
 				url: url,
-				path: `Documentation / ${currentPath}`,
+				path: `PL/SQL Documentation / ${currentPath}`,
 				category: 'APEX Documentation',
 				context: 'app'
 			};
-			
+
 			// Add to results
 			results.push(docObject);
-			
+
 			// Process nested topics recursively
 			if (topic.topics && Array.isArray(topic.topics)) {
 				const nestedResults = processTocTopics(topic.topics, currentPath);
 				results.push(...nestedResults);
 			}
 		});
-		
-		return results;
-	}
 
-	/**
-	 * Get initials from a string (up to 2 characters)
-	 * @param {string} str - Input string
-	 * @returns {string} Initials (up to 2 characters)
-	 */
-	function getInitials(str) {
-		if (!str) return '';
-		
-		// Remove any numbers and special characters
-		const cleanStr = str.replace(/[0-9.()\[\]{}]/g, '').trim();
-		
-		// Split by spaces
-		const words = cleanStr.split(' ').filter(word => word.length > 0);
-		
-		if (words.length === 0) return '';
-		
-		if (words.length === 1) {
-			// For single words, take first two characters if possible
-			return words[0].substring(0, 2).toUpperCase();
-		}
-		
-		// For multiple words, take first character of first two words
-		return (words[0][0] + words[1][0]).toUpperCase();
+		return results;
 	}
 
 	// Process the TOC and build the documentation objects
 	const docObjects = [];
-	
+
 	// Check if tocJson has the expected structure
 	if (tocJson && tocJson.toc && Array.isArray(tocJson.toc)) {
 		tocJson.toc.forEach(section => {
@@ -110,116 +86,134 @@
 			}
 		});
 	}
-	
-	console.log('Generated documentation objects:', docObjects);
-	
-	// Example object structure:
-	// {
-	// 	"name": "Travis Test",
-	// 	"initials": "TC",
-	// 	"icon": "<span class=\"a-Spotlight-icon\" aria-hidden=\"true\"><span class=\"a-Icon icon-shared-components\"></span></span>",
-	// 	"url": "https://docs.oracle.com/en/database/oracle/apex/24.2/aeapi/toc.htm",
-	// 	"path": "Documentation / Table of Contents",
-	// 	"category": "APEX.SEARCH.DOC.TABLE_OF_CONTENTS",
-	// 	"context": "app"
-	// }
 
-    const TARGET_URL = 'wwv_flow.ajax';
-    const TARGET_REQUEST = 'APPLICATION_PROCESS%3DspotlightIndex';
-
-    /**
-     * Intercepts fetch requests to the target URL and allows modification of the JSON response.
-     * @param {RequestInfo} input
-     * @param {RequestInit} [init]
-     * @returns {Promise<Response>}
-     */
-    const originalFetch = window.fetch;
-    window.fetch = async function (input, init) {
+	/**
+	 * Intercepts fetch requests to the target URL and allows modification of the JSON response.
+	 * @param {RequestInfo} input
+	 * @param {RequestInit} [init]
+	 * @returns {Promise<Response>}
+	 */
+	const originalFetch = window.fetch;
+	// Note that APEX uses XMLHttpRequest, not fetch as of 24.2, but this should future-proof in case they decide to switch in a minor patch
+	window.fetch = async function (input, init) {
 		console.log('fetch initiated')
-        const url = typeof input === 'string' ? input : input.url;
-        if (url.includes(TARGET_URL) && init && init.body && init.body.toString().includes('p_request=' + TARGET_REQUEST)) {
-            const response = await originalFetch(input, init);
-            const cloned = response.clone();
-            const json = await cloned.json();
+		const url = typeof input === 'string' ? input : input.url;
+		if (url.includes('wwv_flow.ajax') && init && init.body && init.body.toString().includes('p_request=APPLICATION_PROCESS%3DspotlightIndex')) {
+			const response = await originalFetch(input, init);
+			const cloned = response.clone();
+			const json = await cloned.json();
 
-                        // --- MODIFY JSON RESPONSE HERE IF NEEDED ---
-            /** @type {any} */
-            let newJson = json;
-            // Add documentation objects to the global spotlight search results
-            if (docObjects && docObjects.length > 0) {
-                if (!newJson.global) {
-                    newJson.global = [];
-                }
-                newJson.global.push(...docObjects);
-            }
+			let newJson = json;
+			// Add documentation objects to the global spotlight search results
+			if (docObjects && docObjects.length > 0) {
+				if (!newJson.global) {
+					newJson.global = [];
+				}
+				newJson.global.push(...docObjects);
+			}
 
-            return new Response(JSON.stringify(newJson), {
-                status: response.status,
-                statusText: response.statusText,
-                headers: response.headers
-            });
-        }
-        return originalFetch(input, init);
-    };
+			return new Response(JSON.stringify(newJson), {
+				status: response.status,
+				statusText: response.statusText,
+				headers: response.headers
+			});
+		}
+		return originalFetch(input, init);
+	};
 
-    /**
-     * Intercepts XMLHttpRequest POSTs to the target URL and allows modification of the JSON response.
-     * @typedef {XMLHttpRequest & {
-     *   _intercept: boolean,
-     *   _isTargetUrl: boolean,
-     *   _interceptUrl: string
-     * }} InterceptXHR
-     */
-    const originalOpen = window.XMLHttpRequest.prototype.open;
-    const originalSend = window.XMLHttpRequest.prototype.send;
+	/**
+	 * Intercepts XMLHttpRequest POSTs to the target URL and allows modification of the JSON response.
+	 * @typedef {XMLHttpRequest & {
+	 *   _intercept: boolean,
+	 *   _isTargetUrl: boolean,
+	 *   _interceptUrl: string
+	 * }} InterceptXHR
+	 */
+	const originalOpen = window.XMLHttpRequest.prototype.open;
+	const originalSend = window.XMLHttpRequest.prototype.send;
 
-    /**
-     * @this {InterceptXHR}
-     */
-    window.XMLHttpRequest.prototype.open = function (method, url, ...rest) {
+	/**
+	 * @this {InterceptXHR}
+	 */
+	window.XMLHttpRequest.prototype.open = function (method, url, ...rest) {
 		console.log('xhr open')
-        this._intercept = false; // Will be set to true in send if it matches our criteria
-        this._isTargetUrl = (method.toUpperCase() === 'POST' && url.includes(TARGET_URL));
-        this._interceptUrl = url;
-        return originalOpen.call(this, method, url, ...rest);
-    };
+		this._intercept = false; // Will be set to true in send if it matches our criteria
+		this._isTargetUrl = (method.toUpperCase() === 'POST' && url.includes('wwv_flow.ajax'));
+		this._interceptUrl = url;
+		return originalOpen.call(this, method, url, ...rest);
+	};
 
-    /**
-     * @this {InterceptXHR}
-     */
-    window.XMLHttpRequest.prototype.send = function (body) {
+	/**
+	 * @this {InterceptXHR}
+	 */
+	window.XMLHttpRequest.prototype.send = function (body) {
 		console.log('xhr send')
-        // Only intercept if it's our target URL and contains the specific request parameter
-        if (this._isTargetUrl && body && body.toString().includes('p_request=' + TARGET_REQUEST)) {
-            this._intercept = true;
-            this.addEventListener('readystatechange', function () {
-                if (this.readyState === 4 && this.status === 200) {
-                    try {
-                        /** @type {any} */
-                        let json = JSON.parse(this.responseText);
+		// Only intercept if it's our target URL and contains the specific request parameter
+		if (this._isTargetUrl && body && body.toString().includes('p_request=APPLICATION_PROCESS%3DspotlightIndex')) {
+			this._intercept = true;
+			this.addEventListener('readystatechange', function () {
+				if (this.readyState === 4 && this.status === 200) {
+					try {
+						/** @type {any} */
+						let json = JSON.parse(this.responseText);
 
 						// Add documentation objects to the global spotlight search results
-                        if (docObjects && docObjects.length > 0) {
-                            if (!json.global) {
-                                json.global = [];
-                            }
-                            json.global.push(...docObjects);
-                        }
-						console.log({global:json.global})
+						if (docObjects && docObjects.length > 0) {
+							if (!json.global) {
+								json.global = [];
+							}
+							json.global.push(...docObjects);
+						}
 
-                        const newResponse = JSON.stringify(json);
+						const newResponse = JSON.stringify(json);
 
-                        // Redefine responseText and response (read-only, so use Object.defineProperty)
-                        Object.defineProperty(this, 'responseText', { value: newResponse });
-                        Object.defineProperty(this, 'response', { value: newResponse });
-                    } catch (e) {
-                        // If not JSON, do nothing
+						// Redefine responseText and response (read-only, so use Object.defineProperty)
+						Object.defineProperty(this, 'responseText', { value: newResponse });
+						Object.defineProperty(this, 'response', { value: newResponse });
+					} catch (e) {
+						// If not JSON, do nothing
 						console.error('not json' + e);
-                    }
-                }
-            });
-        }
-        return originalSend.call(this, body);
-    };
+					}
+				}
+			});
+		}
+		return originalSend.call(this, body);
+	};
 
+
+	// Listen for search results to appear in the spotlight
+	document.arrive('.ui-dialog--apexspotlight', { existing: true, onceOnly: true }, (el) => {
+		console.log('search results appear')
+		el.arrive('.a-Spotlight-result[data-category=apex_documentation]', { fireOnAttributesModification: true}, (result) => {
+			/*
+			<li id="sp-result-0" aria-selected="false" role="option" class="a-Spotlight-result a-Spotlight-result--page" data-category="apex_documentation" aria-label="Go to 43.4  GET_URL Function, Documentation / 43  APEX_PAGE / 43.4  GET_URL Function, Without alias, Without shortcut" data-index="0" is-visible="true">
+				<span class="a-Spotlight-link" data-url="https://docs.oracle.com/en/database/oracle/apex/24.2/aeapi/GET_URL-Function.html#GUID-83A2A2A0-5B43-4A3E-BBD3-2FBD3B7B01CE">
+					<span class="a-Spotlight-icon" aria-hidden="true"><span class="fa fa-question-circle-o" aria-hidden="true"></span></span>
+					<span class="a-Spotlight-info">
+					<span class="a-Spotlight-label" aria-hidden="true">43.4  <span class="a-Spotlight-highlight">GET_URL</span> Function</span>
+					<span class="a-Spotlight-desc" aria-hidden="true">Documentation / 43  APEX_PAGE / 43.4  GET_URL Function</span>
+					</span>
+				</span>
+			</li>
+			*/
+			console.log('spotlight result found. replacing with link')
+			const url = result.querySelector('span.a-Spotlight-link').dataset.url
+			const link = document.createElement('a')
+			link.href = url
+			link.target = '_blank'
+			link.classList.add('a-Spotlight-link')
+			link.innerHTML = result.querySelector('span.a-Spotlight-link').innerHTML
+			result.querySelector('span.a-Spotlight-link').replaceWith(link)
+		})
+		document.querySelector('.a-Spotlight-input')?.addEventListener('keydown', (e) => {
+			console.log('keydown on spotlight input', e.key)
+			if (e.key === 'Enter') {
+				e.preventDefault()
+				const activeLink = document.querySelector('.a-Spotlight-result[data-category=apex_documentation].is-active:has(a.a-Spotlight-link)')
+				if (activeLink) {
+					activeLink.querySelector('a.a-Spotlight-link')?.click()
+				}
+			}
+		})
+	})
 })();
